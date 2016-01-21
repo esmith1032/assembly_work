3266ae3f3315fec1aa5fb976490486bd6b634e0d
esmith1032 <esmit013@email.ucr.edu>
2016-01-20 22:50:05 -0800

    Update get_qual_stats.sh

--- get_qual_stats.sh
+++ get_qual_stats.sh
@@ -1,4 +1,3 @@
-```
 module load fastqc
 
 gunzip /bigdata/bioinfo/esmit013/virus_work/flowcell315_lane1_pair1_CTTGTA.fastq.gz
@@ -20,4 +19,3 @@ gzip /bigdata/bioinfo/esmit013/virus_work/flowcell315_lane2_pair1_CTTGTA.fastq
 gunzip /bigdata/bioinfo/esmit013/virus_work/flowcell315_lane2_pair1_GCCAAT.fastq.gz
 fastqc --outdir /bigdata/bioinfo/esmit013/virus_work/fastqc /bigdata/bioinfo/esmit013/virus_work/flowcell315_lane2_pair1_GCCAAT.fastq 
 gzip /bigdata/bioinfo/esmit013/virus_work/flowcell315_lane2_pair1_GCCAAT.fastq
-```
