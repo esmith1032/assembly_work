5fdd564b9ad10af87dd026fe13d93d3a594500d3
esmith1032 <esmit013@email.ucr.edu>
2016-02-02 11:36:24 -0800

    Delete run_soapdenovo.sh

--- fungal_assembly/run_soapdenovo.sh
+++
@@ -1,7 +0,0 @@
-module load SOAPdenovo2
-SOAPdenovo-127mer all -s /bigdata/bioinfo/esmit013/fungal_assembly/soap_raw_data/soap.config -K 63 -o /bigdata/
-bioinfo/esmit013/fungal_assembly/soap_raw_data/soapgraph
-
-##for processed data:
-SOAPdenovo-127mer all -s /bigdata/bioinfo/esmit013/fungal_assembly/soap_processed_data/soap.config -K 63 -o /bigdata/
-bioinfo/esmit013/fungal_assembly/soap_processed_data/soapgraph
