e73b3ce7ad6323c46879acc3922346c32690291b
esmith1032 <esmit013@email.ucr.edu>
2016-05-04 11:00:48 -0700

    Update make_cazy_heatmaps.R

--- fungal_assembly/CAZy/make_cazy_heatmaps.R
+++ fungal_assembly/CAZy/make_cazy_heatmaps.R
@@ -1,13 +1,26 @@
 setwd("/Research/genomics/fungal_assembly/CAZy_output")
 cats <- c('AA', 'CBM', 'CE', 'GH', 'GT', 'PL')
-cols <- colorRampPalette(c("red", "yellow", "green"))(n = 299)
+##cols <- colorRampPalette(c("red", "yellow", "green"))(n = 299)
+cols <- colorRampPalette(c("red", "black", "green"))(n = 299)
 pdf("CAZy_heatmaps.pdf", height=10.5, width=8)
 for (i in 1:length(cats))
   {
   cat <- cats[i]  
+  if (cat == "AA")
+    {title <- "Auxillary Activities"}
+  if (cat == "CBM")
+    {title <- "Carbohydrate Binding Modules"}
+  if (cat == "CE")
+    {title <- "Carbohydrate Esterases"}
+  if (cat == "GH")
+    {title <- "Glycoside Hydrolases"}
+  if (cat == "GT")
+    {title <- "Glycosyl Transferases"}
+  if (cat == "PL")
+    {title <- "Polysaccharide Lyases"}
   file <- paste(cat, "_matrix.txt", sep="")  
   mat <- as.matrix(read.table(file, header=TRUE))
-  heatmap(mat, col=cols, scale="column", Colv=NA, Rowv=NA, main=cat, xlab="CAZy model", ylab="Species")  
+  heatmap(mat, col=cols, scale="column", Colv=NA, Rowv=NA, main=title, xlab="CAZy model", ylab="Species")  
   }
 dev.off()
 
@@ -15,8 +28,20 @@ pdf("py_CAZy_heatmaps.pdf", height=10.5, width=8)
 for (k in 1:length(cats))
   {
   cat <- cats[k]  
+  if (cat == "AA")
+    {title <- "Auxillary Activities"}
+  if (cat == "CBM")
+    {title <- "Carbohydrate Binding Modules"}
+  if (cat == "CE")
+    {title <- "Carbohydrate Esterases"}
+  if (cat == "GH")
+    {title <- "Glycoside Hydrolases"}
+  if (cat == "GT")
+    {title <- "Glycosyl Transferases"}
+  if (cat == "PL")
+    {title <- "Polysaccharide Lyases"}
   file <- paste("py_", cat, "_matrix.txt", sep="")  
   mat <- as.matrix(read.table(file, header=TRUE))
-  heatmap(mat, col=cols, scale="column", Colv=NA, Rowv=NA, main=cat, xlab="CAZy model", ylab="Species")  
+  heatmap(mat, col=cols, scale="column", Colv=NA, Rowv=NA, main=title, xlab="CAZy model", ylab="Species")  
   }
 dev.off()
